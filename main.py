d760c90d866a4ba225c550a3904784cfc69ba5ce
Trimpta <68428753+trimpta@users.noreply.github.com>
2023-11-18 07:13:00 +0530

    some fixes

--- main.py
+++ main.py
@@ -5,13 +5,16 @@
 import os
 import time
 
+def date_printable():
+    return f"{time.localtime()[2]}-{time.localtime()[1]}-{time.localtime()[0]}" #"date-month-year"
+
 def time_printable():
-    time_string = f"{time.localtime()[3]} {time.localtime()[4]}-{time.localtime()[5]} {time.localtime()[2]}-{time.localtime()[1]}"
-    return time_string
+    return f"{time.localtime()[3]}:{time.localtime()[4]}:{time.localtime()[5]}" #hour:minute:second
+
 def get_file_list():
     return [f for f in os.listdir(os.getcwd()) if os.path.isfile(f)]
 
-def createFolder(folder:str):
+def create_folder(folder:str):
     path=os.path.join(os.getcwd(),folder)
     os.mkdir(folder)
     print(f"Created new folder:{folder}")
@@ -21,17 +24,19 @@ def movefile(file:str,folder:str):
     name_new = file[file.find("]")+1::]
     destination = os.path.join(os.getcwd(),folder,name_new)
     if os.path.exists(destination):
-        destination = os.path.join(os.getcwd(),folder,name_new,"(Copy)")
-
+        prefix=file[file.rfind('.')::]
+        name_copy = name_new[0:file.rfind('.')]
+        destination = os.path.join(os.getcwd(),folder,name_copy,"(Copy)",prefix)
+        print("File already exists. Appending (copy) to file name.")
     os.rename(file,destination)
 
     print(f"Moved   {file} to {destination}.")
     #moves {file} from working dir to dir/folder after removing the [folder] prefix
 
-log_file=f"folderizer-log-{time_printable()}.txt"
+log_file=f"folderizer-log-{date_printable() }.txt"
 
 with open(log_file,"a") as f:
-    f.write(f"Currently executing program in {os.getcwd()}\n")
+    f.write(f"Currently executing program in {os.getcwd()}\nTime:{time.ctime()}")
     print(f"Currently executing program in {os.getcwd()}")
     while True:
         
@@ -48,19 +53,15 @@ with open(log_file,"a") as f:
                             raise ValueError("sorry bro ur dum dum")
                         else:
                             print(f"Error: You have tried to create a folder which already exists as a file.")
-                            f.write(f"An error occurred: User attempted to create folder which already exists as a file.")
+                            f.write(f"{date_printable()} {time_printable()} An error occurred: User attempted to create folder which already exists as a file.")
                     else:
-                        createFolder(folder)
+                        create_folder(folder)
 
                     time.sleep(0.1)       #for mitigating windows error
                     movefile(i,folder)
-                    f.write(f'{time_printable()}:       Moved       {i.lstrip(i[0:i.find("]")+1])} to {folder}.\n')
+                    f.write(f'{date_printable()} {time_printable()}:       Moved       {i.lstrip(i[0:i.find("]")+1])} to {folder}.\n')
 
-                except KeyboardInterrupt:
-                    print("Exiting")
-                    f.write(time_printable(),"  Program closed.")
-                    f.close()
                 except Exception as Error:
-                    print(f"{time_printable()}  An error occurred: {Error}")
-                    f.write(f"{time_printable()}    An error occurred: {Error}\n")
-        time.sleep(10)
+                    print(f"{date_printable()}  An error occurred: {Error}")
+                    f.write(f"{date_printable()} {time_printable()}    An error occurred: {Error}\n")
+        time.sleep(60)
